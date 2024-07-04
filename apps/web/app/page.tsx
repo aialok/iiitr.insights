002ffc19a90ee2fb3095ec1eff31eef7d5f62052
Alok Gupta <alokgupta1560@gmail.com>
2024-07-04 23:45:28 +0530

    First Commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+        <h1>iiitr.ai</h1>
+    </div>
+  );
+}
